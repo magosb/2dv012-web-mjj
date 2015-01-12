bc7205eff82c648bc98a1dfb4536c37d4027fe39
Jerry Strand <js222yj@student.lnu.se>
2015-01-12 23:33:52 +0100

    Working DataService and test page data.xhtml

--- src/se/lnu/xmascal/DataService.java
+++ src/se/lnu/xmascal/DataService.java
@@ -15,10 +15,12 @@ import java.io.Serializable;
 // TODO: Need to validate all request parameters
 
 /**
+ * This class is responsible for making binary database data available in a number of ways.
+ *
  * @author Jerry Strand
  */
 @Named
-@ApplicationScoped// TODO: Should be application scoped
+@ApplicationScoped
 public class DataService implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -28,23 +30,11 @@ public class DataService implements Serializable {
     public DataService() {
     }
 
-
-
-
-
-
-    // TODO: For future: cal ( thumbn | bg | (wcontent day) )
-    // ^ request parameters set can be calendar name, followed by the type of file/binary, followed by either type bg/thumbnail or type window content followed by the day
-    // Define public constants in this class to prevent typos?
-    // TODO: ^ NO! Define these methods:
-    // getCalendarBackground: param cal
-    // getCalendarThumbnail: param cal
-    // getWindowContent param cal, param day
-
     /**
-     * Reads request param "cal"
-     *
-     * @return
+     * When this method is called from a view that is being rendered, it returns stub data with the sole responsibility
+     * of generating a valid URL.<br>
+     * When the client browser uses the generated URL to request the image, this method will check for a request
+     * parameter "cal" and return the actual thumbnail data of the <code>Calendar</code> with that name.
      */
     public synchronized StreamedContent getCalendarThumbnail() {
         FacesContext context = FacesContext.getCurrentInstance();
@@ -57,22 +47,25 @@ public class DataService implements Serializable {
         // Image is being requested. Return a real StreamedContent with the image bytes.
         else {
             String calName = context.getExternalContext().getRequestParameterMap().get("cal");
-            //Calendar calendar = calendarManager.getCalendar(calName);
             return new ByteArrayContent(getCalendarThumbnail(calName));
         }
     }
 
+    /**
+     * @param calendarName the name of the <code>Calendar</code> whose thumbnail shall be retrieved
+     * @return the binary data that represents the thumbnail
+     */
     private byte[] getCalendarThumbnail(String calendarName) {
-        System.out.println("GETTING THUMBNAIL");
         Query query = em.createQuery("SELECT c.thumbnail FROM Calendar c WHERE c.name = :cname");
         query.setParameter("cname", calendarName);
         return (byte[]) query.getSingleResult();
     }
 
     /**
-     * Reads request param "cal"
-     *
-     * @return
+     * When this method is called from a view that is being rendered, it returns stub data with the sole responsibility
+     * of generating a valid URL.<br>
+     * When the client browser uses the generated URL to request the image, this method will check for a request
+     * parameter "cal" and return the actual background data of the <code>Calendar</code> with that name.
      */
     public synchronized StreamedContent getCalendarBackground() {
         FacesContext context = FacesContext.getCurrentInstance();
@@ -85,12 +78,14 @@ public class DataService implements Serializable {
         // Image is being requested. Return a real StreamedContent with the image bytes.
         else {
             String calName = context.getExternalContext().getRequestParameterMap().get("cal");
-            System.out.println("SERVICE, name = " + calName);
-            //Calendar calendar = calendarManager.getCalendar(calName);
             return new ByteArrayContent(getCalendarBackground(calName));
         }
     }
 
+    /**
+     * @param calendarName the name of the <code>Calendar</code> whose background shall be retrieved
+     * @return the binary data representing the background
+     */
     private byte[] getCalendarBackground(String calendarName) {
         System.out.println("GETTING BACKGROUND");
         Query query = em.createQuery("SELECT c.background FROM Calendar c WHERE c.name = :cname");
@@ -98,7 +93,14 @@ public class DataService implements Serializable {
         return (byte[]) query.getSingleResult();
     }
 
-    public synchronized StreamedContent getWindowContent() {
+    /**
+     * This method is used to retrieve <b>non text</b> content of a <code>Calendar Window</code>.<br>
+     * When this method is called from a view that is being rendered, it returns stub data with the sole responsibility
+     * of generating a valid URL.<br>
+     * When the client browser uses the generated URL to request the content, this method will check for request
+     * parameters "cal" and "day" and return the actual content of that <code>Calendar Window</code>.
+     */
+    public synchronized StreamedContent getStreamedContent() {
         FacesContext context = FacesContext.getCurrentInstance();
 
         // View is being rendered. Return a stub StreamedContent so that it will generate right URL.
@@ -110,15 +112,30 @@ public class DataService implements Serializable {
         else {
             String calName = context.getExternalContext().getRequestParameterMap().get("cal");
             int winDay = Integer.parseInt(context.getExternalContext().getRequestParameterMap().get("day"));
-            //Calendar calendar = calendarManager.getCalendar(calName);
             return new ByteArrayContent(getWindowContent(calName, winDay));
         }
     }
 
+    /**
+     * This method is used to retrieve text (includes URL) content of a <code>Calendar Window</code>.<br>
+     * When this method is called from a view that is being rendered, it returns stub data with the sole responsibility
+     * of generating a valid URL.<br>
+     * When the client browser uses the generated URL to request the content, this method will check for request
+     * parameters "cal" and "day" and return the actual content of that <code>Calendar Window</code>.
+     */
+    public synchronized String getTextContent(String calName, int winDay) {
+        return new String(getWindowContent(calName, winDay));
+    }
+
+    /**
+     * @param calendarName the name of the <code>Calendar</code> whose <code>Window</code> content to retrieve
+     * @param day the day of the <code>Window</code>
+     * @return the binary data representing the <code>Window</code> content
+     */
     private byte[] getWindowContent(String calendarName, int day) {
-        System.out.println("GETTING CONTENT");
         Query query = em.createQuery("SELECT w.content FROM Window w WHERE w.calendarName = :cname AND w.day = :wday");
         query.setParameter("cname", calendarName).setParameter("wday", day);
         return (byte[]) query.getSingleResult();
     }
+
 }
