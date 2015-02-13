f16784de37edd4747c185cea7f7ca30b50bc4743
Jerry Strand <js222yj@student.lnu.se>
2015-02-13 16:27:28 +0100

    Cleaned up AddCalendarBean

--- src/se/lnu/xmascal/AddCalendarBean.java
+++ src/se/lnu/xmascal/AddCalendarBean.java
@@ -5,7 +5,6 @@ import se.lnu.xmascal.ejb.CalendarManager;
 import se.lnu.xmascal.model.Calendar;
 import se.lnu.xmascal.model.Window;
 
-import javax.annotation.PreDestroy;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -17,16 +16,12 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-
-// <!-- <h:selectOneRadio id="isPublic" value="#{newCalendar.public}" onchange="submit()" valueChangeListener="#{newCalendar.update}"> TODO: This will submit entire form? Change to separate form? -->
-//<!-- <h:inputText id="passPhrase" value="#{newCalendar.passPhrase}" disabled="#{newCalendar.public}"/> -->
-
 /**
- * This class is a ViewScoped Managed Bean for the Calendar class.
+ * This class is a ViewScoped Managed Bean for the addition of calendars to the application.
  *
  * @author Jerry Strand
  */
-//@DeclareRoles("XmasCalAdmin")
+//@DeclareRoles("XmasCalAdmin") // TODO: Skip this?
 @Named("newCalendar")
 @ViewScoped
 public class AddCalendarBean implements Serializable {
@@ -41,37 +36,44 @@ public class AddCalendarBean implements Serializable {
     private Calendar calendar = new Calendar();
     private boolean isPublic = true;
 
-    // TODO: Save Calendar in DB when next button is clicked (needed to get ID used for redirection to edit page).
-    // Set baseConfigured to true when save button is clicked. Add @PreDestroy method to this bean to remove calendar from DB if baseConfigured is false
-    private boolean baseConfigured = false;
-
-    @PreDestroy
-    private void cleanUp() { // TODO: Actually, this should be done when the save button is clicked?
-        if (!baseConfigured) { // Name, background, thumbnail weren't entered
-            // TODO: Remove from DB, set background/thumbnail to null
-        }
-    }
-
+    /**
+     * @return the name that the new calendar shall receive
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name the name that the new calendar shall receive
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return <code>true</code> if the calendar shall be public, otherwise <code>false</code>
+     */
     public boolean getPublic() {
         return isPublic;
     }
 
+    /**
+     * @param isPublic whether the calendar shall be public or not
+     */
     public void setPublic(boolean isPublic) {
         this.isPublic = isPublic;
     }
 
+    /**
+     * @return the passphrase for the new calendar
+     */
     public String getPassPhrase() {
         return passPhrase;
     }
 
+    /**
+     * @param passPhrase the passphrase for the new calendar
+     */
     public void setPassPhrase(String passPhrase) {
         this.passPhrase = passPhrase;
     }
@@ -97,8 +99,7 @@ public class AddCalendarBean implements Serializable {
      */
     public void handleThumbnailUpload(FileUploadEvent event) {
         try {
-            // event.getFile().getContents(); // TODO: Not needed?
-            thumbnail = getUploadedBytes(event.getFile().getInputstream()); // TODO: Test what happens if Save is clicked while upload is being done
+            thumbnail = getUploadedBytes(event.getFile().getInputstream());
             sendInfoMsg(event.getFile().getFileName() + " has been uploaded.");
         } catch (IOException e) {
             sendErrorMsg("Unable to retrieve uploaded thumbnail data");
@@ -111,14 +112,15 @@ public class AddCalendarBean implements Serializable {
      */
     private byte[] getUploadedBytes(InputStream is) throws IOException {
         byte[] data = new byte[is.available()]; // TODO: THIS MIGHT NOT READ ALL BYTES!!
-        is.read(data);
+        is.read(data); // TODO: THIS MAY BE THE SOURCE OF SOME PROBLEMS!!
         is.close();
         return data;
     }
 
     /**
-     * Adds the managed calendar. Error messages are sent to the current facelet if any attributes are null and if a
-     * calendar with the set name exists. An info message is sent if the calendar was added successfully.
+     * This method stores the entered data as a calendar in the database. Error messages are sent to the current facelet
+     * if any attributes are null and if a calendar with the set name exists. An info message is sent if the calendar
+     * was added successfully.
      */
     public String save() {
         if (isPublic) {
@@ -132,43 +134,42 @@ public class AddCalendarBean implements Serializable {
             return null;
         }
 
-        calendar = new Calendar(name, background, thumbnail, passPhrase);
+        // Set default content in all windows
         List<Window> windows = new ArrayList<>();
         byte[] defaultMessage = "No content has been added to this window.".getBytes();
         for (int i = 1; i <= 24; i++) {
-            windows.add(new Window(calendar.getName(), i, defaultMessage, Window.ContentType.TEXT));
+            windows.add(new Window(name, i, defaultMessage, Window.ContentType.TEXT));
         }
-        calendar.setWindows(windows);
 
+        calendar = new Calendar(name, background, thumbnail, passPhrase);
+        calendar.setWindows(windows);
         calendarManager.add(calendar);
         sendInfoMsg("Calendar has been added.");
 
-        calendar = calendarManager.getCalendar(name); // Needed to get correct numeric ID
-        String outcome = "/admin/edit-calendar/index.xhtml?faces-redirect=true&amp;cal=" + calendar.getNumericId(); //
-        System.out.println("Returning outcome " + outcome);
-        return outcome; // TODO: Update this to /admin/edit-calendar/ -- also need to pass parameter detailing which calendar
+        calendar = calendarManager.getCalendar(name); // Sadly needed to get correct numeric ID
+        return "/admin/edit-calendar/index.xhtml?faces-redirect=true&amp;cal=" + calendar.getNumericId();
     }
 
 
     /**
-     * Checks for <code>null</code> attributes and if found, sends error message(s) to the current facelet.
+     * Checks for <code>null</code> attributes, empty name and (if calendar shall be private) empty passphrase. If any
+     * such errors are found,  error message(s) are sent to the current facelet.
      *
-     * @return <code>true</code> if any attributes are <code>null</code>
+     * @return <code>true</code> if any errors are found
      */
     private boolean hasNullErrors() {
         boolean hasErrors = false;
 
-        if (name == null || name.isEmpty()) { // TODO: Need better validation: empty String? Too short? Etc
+        if (name == null || name.isEmpty()) {
             sendErrorMsg("Invalid name.");
             hasErrors = true;
         }
-        if (background == null) {// TODO: Need better validation: empty String? Too short? Etc
+        if (background == null) {
             sendErrorMsg("Invalid background.");
             hasErrors = true;
         }
-        if (thumbnail == null) {// TODO: Need better validation: empty String? Too short? Etc
-            FacesContext.getCurrentInstance().validationFailed();
-            // Add an appropriate faces message which can be displayed in a message component which autoupdates for instance.
+        if (thumbnail == null) {
+            FacesContext.getCurrentInstance().validationFailed(); // TODO: This needs some testing! Does it work? Can it be made to work? If so, add to all cases?
             sendErrorMsg("Invalid thumbnail.");
             hasErrors = true;
         }
@@ -201,55 +202,3 @@ public class AddCalendarBean implements Serializable {
     }
 
 }
-/*
-    // TODO: Try to make this happen client side instead?
-    public String onFlowProcess(FlowEvent event) {
-        System.out.println("New step: " + event.getNewStep());
-        System.out.println(name + passPhrase);
-        return event.getNewStep();
-    }
-
-    public void update(ValueChangeEvent event) {
-        System.out.print("isPublic was '" + isPublic + "'");
-        isPublic = (Boolean) event.getNewValue();
-        System.out.println(", now changed to '" + isPublic + "'");
-    }
-
-    /**
-     * Updates the managed calendar. Error messages are sent to the current facelet if any attributes are null. An info
-     * message is sent if the calendar was updated successfully.
-     */
-// public void update(ValueChangeEvent e) TODO: Might be able to use this instead/in conjunction?
-//public void update() { // TODO: calendar.set... may not work, since the Calendar may be detached. Need to use em.merge
-//    if (hasNullErrors()) {
-//        return;
-//    }
-//    if (calendar == null) {
-//        calendar = calendarManager.getCalendar(name);
-//    }
-//    calendar.setName(name);
-//    calendar.setBackground(background);
-//    calendar.setThumbnail(thumbnail);
-//    calendar.setPassPhrase(passPhrase);
-//    sendInfoMsg("Calendar has been updated.");
-//}
-
-    /*
-     *
-     *
-    public void remove() { // TODO: Not needed?
-        if (name == null) {
-            sendErrorMsg("Name of a calendar is needed to remove it!");
-        } else if (!calendarManager.exists(name)) {
-            sendErrorMsg("No calendar with the name '" + name + "' exists!");
-        } else if (calendar == null) {
-            calendar = calendarManager.getCalendar(name);
-        }
-        calendarManager.remove(calendar);
-        name = null;
-        background = null;
-        thumbnail = null;
-        passPhrase = null;
-        sendInfoMsg("Calendar has been removed.");
-    }
-        */
